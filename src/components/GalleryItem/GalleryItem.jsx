56c93160390f7b6e90a1feb24d5ec7b6212ce865
Raveryk <raveryk@gmail.com>
2021-04-30 18:41:43 -0500

    Got photo to conditionally render AND toggle back and forth on clicks.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,20 +4,38 @@ import {useState} from 'react';
 
 function GalleryItem({image, upLikes}) {
 
-    let [imageClick, setImageClick] = useState(true)
+    let [imageClick, setImageClick] = useState(false)
+    // let [clickCounter, setClickCounter] = useState(0)
          
     const toggleImage = () => {
         console.log('You clicked the image');
-
-        //set state
-        setImageClick(false);
-
+        // clickCounter += 1;
+        console.log(imageClick);
+        // //set state
+        if( imageClick === false ) {
+        setImageClick(true)
+        } else {
+        setImageClick(false)
+        }
     }
+
+    // onOrOffDuty = () => {
+    //     if(hero.onDuty) {
+    //         // need to return JSX
+    //         return <p>ON DUTY</p>;
+    //     } else {
+    //         // need to return JSX
+    //         return  <p>SLEEPING</p>;
+    //     }
+    // }
+
+
+    // !imageClick ? <img src={image.path} width="200" height="200"/> : <p>{image.description}</p>
 
     
     return(
 
-        <div className="images" onClick={(event) => toggleImage()} ><div>{ imageClick ? <img src={image.path} width="200" height="200"/> : <p>{image.description}</p>}</div>
+        <div className="images" onClick={(event) => toggleImage()} ><div>{ !imageClick ? <img src={image.path} width="200" height="200"/> : <p>{image.description}</p> }</div>
         <p><button type="submit" onClick={(event) => upLikes(image)}>Love It!!</button> {image.likes} people love this!</p></div>
     )}
 
