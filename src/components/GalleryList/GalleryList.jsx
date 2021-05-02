142adb137958ba79d54fc2fa9ed3415c94b2e769
Raveryk <raveryk@gmail.com>
2021-05-02 16:37:55 -0500

    Used Material UI to  format image spacing.

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,16 +1,17 @@
 import GalleryItem from '../GalleryItem/GalleryItem';
-import './GalleryList.css'
+import './GalleryList.css';
+import Grid from '@material-ui/core/Grid';
 
 // Component to iterate through the galleryList
 function GalleryList({ galleryList, upLikes, deleteImage }) {
 
 
     return(
-        <div className="images">
+        <Grid container spacing ={4} className="images">
             {galleryList.map(image => 
-            (<GalleryItem key={image.id} image={image} upLikes={upLikes} deleteImage={deleteImage} />)
+            (<Grid justify="center" alignItems="center" direction ="row" container item sm={4} spacing={1}><GalleryItem key={image.id} image={image} upLikes={upLikes} deleteImage={deleteImage} /></Grid>)
             )}
-        </div>
+        </Grid>
     )
 
 
